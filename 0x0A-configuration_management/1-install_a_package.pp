f1d79a71db9519de8eb6e80f7d8f02aa0a9f0a04
Oscar Akwantey Boafo <141040248+regalconcepts@users.noreply.github.com>
2024-04-19 22:23:51 -0700

    added files for configuration management project

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
